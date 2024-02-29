ae5cae8546527be7ffbf73f10b9847a4f7245cf0
iziKomSD <122015463+iziKomSD@users.noreply.github.com>
2024-02-29 12:54:41 +0200

    start commit

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,20 +1,19 @@
-export function getImages(query) {
-  const baseUrl = 'https://pixabay.com/api/';
-  const keyApi = '26468965-37c536f46ba330a607460f03f';
+import axios from 'axios';
 
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const keyApi = '26468965-37c536f46ba330a607460f03f';
+
+export async function getImages(query, perPage, pageNum) {
   const queryParams = new URLSearchParams({
     key: keyApi,
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-    per_page: 30,
+    per_page: perPage,
+    page: pageNum,
   });
 
-  return fetch(`${baseUrl}?${queryParams}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  const response = await axios.get('', { params: queryParams });
+  return response.data;
 }
